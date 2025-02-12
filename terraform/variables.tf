7131e1d7ddf9d3c9d5b22a36a75ac492374f97a3
spiderweb1801 <33922845+spiderweb1801@users.noreply.github.com>
2025-02-12 19:24:54 +0530

    feat oidc

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,24 +5,24 @@ locals {
       subnet1 = {
         name              = "public-subnet-1"
         cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, 0)
-        availability_zone = "${data.aws_region.current}a"
+        availability_zone = "${data.aws_region.current.name}a"
       },
       subnet2 = {
         name              = "public-subnet-2"
         cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, 1)
-        availability_zone = "${data.aws_region.current}b"
+        availability_zone = "${data.aws_region.current.name}b"
       }
     },
     private_subnets = {
       subnet1 = {
         name              = "private-subnet-1"
         cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, 0)
-        availability_zone = "${data.aws_region.current}a"
+        availability_zone = "${data.aws_region.current.name}a"
       },
       subnet2 = {
         name              = "private-subnet-2"
         cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, 1)
-        availability_zone = "${data.aws_region.current}b"
+        availability_zone = "${data.aws_region.current.name}b"
       }
     }
   }
