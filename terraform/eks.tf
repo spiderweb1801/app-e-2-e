f9d6e3f424a1e53e7c888e0cb3c1c0a666fb1216
spiderweb1801 <33922845+spiderweb1801@users.noreply.github.com>
2025-02-12 20:49:48 +0530

    feat oidc

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -3,7 +3,7 @@ resource "aws_eks_cluster" "eks_cluster" {
   role_arn = aws_iam_role.eks_role.arn
 
   vpc_config {
-    subnet_ids = [ aws_subnet.private_subnets[].id ]
+    subnet_ids = [ for i in aws_subnet.private_subnets: i.id ]
   }
 }
 
@@ -11,7 +11,7 @@ resource "aws_eks_fargate_profile" "fargate_profile" {
   cluster_name           = aws_eks_cluster.eks_cluster.name
   fargate_profile_name   = "hello-world-fargate"
   pod_execution_role_arn = aws_iam_role.fargate_pod_execution.arn
-  subnet_ids             = [ aws_subnet.private_subnets[].id ]
+  subnet_ids             = [ for i in aws_subnet.private_subnets: i.id ]
   selector {
     namespace = "hello-world"
   }
