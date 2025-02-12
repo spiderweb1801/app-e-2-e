c0a73ed1437464979725367f617679b0dbd1d64a
spiderweb1801 <33922845+spiderweb1801@users.noreply.github.com>
2025-02-12 20:37:34 +0530

    feat oidc

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,3 +1,3 @@
-# resource "aws_ecr_repository" "hello_world_repo" {
-#   name = "hello-world"
-# }
+resource "aws_ecr_repository" "hello_world_repo" {
+  name = "hello-world"
+}
--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,45 +1,45 @@
-# resource "aws_iam_role" "eks_role" {
-#   name = "eks-cluster-role"
+resource "aws_iam_role" "eks_role" {
+  name = "eks-cluster-role"
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "eks.amazonaws.com"
-#         }
-#       }
-#     ]
-#   })
-# }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
 
-# resource "aws_iam_policy_attachment" "eks_cluster_policy" {
-#   name       = "eks-cluster-policy-attachment"
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-#   roles      = [aws_iam_role.eks_role.name]
-# }
+resource "aws_iam_policy_attachment" "eks_cluster_policy" {
+  name       = "eks-cluster-policy-attachment"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  roles      = [aws_iam_role.eks_role.name]
+}
 
-# resource "aws_iam_role" "fargate_pod_execution" {
-#   name = "eks-fargate-pod-execution-role"
+resource "aws_iam_role" "fargate_pod_execution" {
+  name = "eks-fargate-pod-execution-role"
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "eks-fargate-pods.amazonaws.com"
-#         }
-#         Action = "sts:AssumeRole"
-#       }
-#     ]
-#   })
-# }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks-fargate-pods.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
 
-# resource "aws_iam_policy_attachment" "fargate_execution_role_policy" {
-#   name       = "fargate-execution-role-attachment"
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
-#   roles      = [aws_iam_role.fargate_pod_execution.name]
-# }
+resource "aws_iam_policy_attachment" "fargate_execution_role_policy" {
+  name       = "fargate-execution-role-attachment"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  roles      = [aws_iam_role.fargate_pod_execution.name]
+}
