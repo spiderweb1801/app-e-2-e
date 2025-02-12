70af6d3957490b8eb30702fbc2da0c2c08928cdf
spiderweb1801 <33922845+spiderweb1801@users.noreply.github.com>
2025-02-12 20:34:37 +0530

    feat oidc

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -18,76 +18,76 @@ resource "aws_internet_gateway" "igw" {
 
 data "aws_region" "current" {}
 
-# resource "aws_subnet" "public_subnets" {
-#   for_each                = local.vpc.public_subnets
-#   vpc_id                  = aws_vpc.main.id
-#   cidr_block              = each.value.cidr_block
-#   map_public_ip_on_launch = true
-#   availability_zone       = each.value.availability_zone
+resource "aws_subnet" "public_subnets" {
+  for_each                = local.vpc.public_subnets
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = each.value.cidr_block
+  map_public_ip_on_launch = true
+  availability_zone       = each.value.availability_zone
+
+  tags = {
+    Name = each.value.name
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  for_each          = local.vpc.private_subnets
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = each.value.cidr_block
+  availability_zone = each.value.availability_zone
+
+  tags = {
+    Name = each.value.name
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  for_each       = aws_subnet.public_subnets
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# resource "aws_eip" "nat" {
+#   domain = "vpc"
+# }
+
+# resource "aws_nat_gateway" "nat" {
+#   allocation_id = aws_eip.nat.id
+#   subnet_id     = aws_subnet.public_1.id
 
 #   tags = {
-#     Name = each.value.name
+#     Name = "nat-gateway"
 #   }
 # }
 
-# resource "aws_subnet" "private_subnets" {
-#   for_each          = local.vpc.private_subnets
-#   vpc_id            = aws_vpc.main.id
-#   cidr_block        = each.value.cidr_block
-#   availability_zone = each.value.availability_zone
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
 
-#   tags = {
-#     Name = each.value.name
-#   }
-# }
+  #   route {
+  #     cidr_block     = "0.0.0.0/0"
+  #     nat_gateway_id = aws_nat_gateway.nat.id
+  #   }
 
-# resource "aws_route_table" "public_rt" {
-#   vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "private-route-table"
+  }
+}
 
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.igw.id
-#   }
-
-#   tags = {
-#     Name = "public-route-table"
-#   }
-# }
-
-# resource "aws_route_table_association" "public" {
-#   for_each       = aws_subnet.public_subnets
-#   subnet_id      = each.value.id
-#   route_table_id = aws_route_table.public_rt.id
-# }
-
-# # resource "aws_eip" "nat" {
-# #   domain = "vpc"
-# # }
-
-# # resource "aws_nat_gateway" "nat" {
-# #   allocation_id = aws_eip.nat.id
-# #   subnet_id     = aws_subnet.public_1.id
-
-# #   tags = {
-# #     Name = "nat-gateway"
-# #   }
-# # }
-
-# resource "aws_route_table" "private_rt" {
-#   vpc_id = aws_vpc.main.id
-
-#   #   route {
-#   #     cidr_block     = "0.0.0.0/0"
-#   #     nat_gateway_id = aws_nat_gateway.nat.id
-#   #   }
-
-#   tags = {
-#     Name = "private-route-table"
-#   }
-# }
-
-# resource "aws_route_table_association" "private" {
-#   for_each       = aws_subnet.private_subnets
-#   subnet_id      = each.value.id
-#   route_table_id = aws_route_table.private_rt.id
-# }
+resource "aws_route_table_association" "private" {
+  for_each       = aws_subnet.private_subnets
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private_rt.id
+}
