719c097f4255a97b003232baeb144da3ea9fb007
spiderweb1801 <33922845+spiderweb1801@users.noreply.github.com>
2025-02-13 18:47:24 +0530

    latest code

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -66,7 +66,7 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public_subnets["public-subnet-1"].id
+  subnet_id     = aws_subnet.public_subnets["subnet1"].id
 
   tags = {
     Name = "nat-gateway"
