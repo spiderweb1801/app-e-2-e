5e8f9bf42a7ccdb698a4584369d0fb715a45dcc2
spiderweb1801 <33922845+spiderweb1801@users.noreply.github.com>
2025-02-13 18:44:39 +0530

    latest code

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -60,26 +60,26 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public_rt.id
 }
 
-# resource "aws_eip" "nat" {
-#   domain = "vpc"
-# }
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
 
-# resource "aws_nat_gateway" "nat" {
-#   allocation_id = aws_eip.nat.id
-#   subnet_id     = aws_subnet.public_1.id
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_subnets["public-subnet-1"].id
 
-#   tags = {
-#     Name = "nat-gateway"
-#   }
-# }
+  tags = {
+    Name = "nat-gateway"
+  }
+}
 
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.main.id
 
-  #   route {
-  #     cidr_block     = "0.0.0.0/0"
-  #     nat_gateway_id = aws_nat_gateway.nat.id
-  #   }
+    route {
+      cidr_block     = "0.0.0.0/0"
+      nat_gateway_id = aws_nat_gateway.nat.id
+    }
 
   tags = {
     Name = "private-route-table"
