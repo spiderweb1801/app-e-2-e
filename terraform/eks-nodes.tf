7fc7c75811df8dfa175473df5d05b5f22986828a
spiderweb1801 <33922845+spiderweb1801@users.noreply.github.com>
2025-02-14 10:01:15 +0530

    new node group in public subnet

--- terraform/eks-nodes.tf
+++ terraform/eks-nodes.tf
@@ -13,3 +13,19 @@ resource "aws_eks_node_group" "eks_nodes" {
     max_unavailable = 1
   }
 }
+
+resource "aws_eks_node_group" "eks_nodes_public" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "public-eks-managed-nodes"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = [ for i in aws_subnet.public_subnets: i.id ] # Update with your VPC subnets
+  instance_types  = ["t3.medium"]
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+  update_config {
+    max_unavailable = 1
+  }
+}
