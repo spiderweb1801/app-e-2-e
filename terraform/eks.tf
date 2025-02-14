091f7b772e06b5a319a16db790753387edb71882
spiderweb1801 <33922845+spiderweb1801@users.noreply.github.com>
2025-02-14 12:58:44 +0530

    new node group in public subnet

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -2,8 +2,9 @@ resource "aws_eks_cluster" "eks_cluster" {
   name     = "hello-world-cluster"
   role_arn = aws_iam_role.eks_role.arn
 
+# cluster should be in public subnet. Ideally it has to be in private subnet
   vpc_config {
-    subnet_ids = [ for i in aws_subnet.private_subnets: i.id ]
+    subnet_ids = [ for i in aws_subnet.public_subnets: i.id ] 
   }
 }
 
