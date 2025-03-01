ad48ac689bd25873f76091508fa4743c245a6627
spiderweb1801 <33922845+spiderweb1801@users.noreply.github.com>
2025-03-01 15:20:28 +0530

    test

--- terraform/eks_nodes_security_groups.tf
+++ terraform/eks_nodes_security_groups.tf
@@ -5,8 +5,8 @@ resource "aws_security_group" "eks_nodes_sg" {
 
   ingress {
     from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    to_port     = 65535
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # Open to all (change for security)
   }
 
--- terraform/eks_security_groups.tf
+++ terraform/eks_security_groups.tf
@@ -3,6 +3,13 @@ resource "aws_security_group" "eks_sg" {
   description = "Communication between the control plane and worker nodegroups"
   vpc_id      = aws_vpc.main.id
 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
