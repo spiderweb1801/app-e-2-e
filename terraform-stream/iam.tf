e7804041c47ff965965cd5718a986131e16d4f22
spiderweb1801 <33922845+spiderweb1801@users.noreply.github.com>
2025-04-04 13:25:54 +0530

    eks push

--- terraform-stream/iam.tf
+++ terraform-stream/iam.tf
@@ -135,34 +135,6 @@ resource "aws_iam_role_policy_attachment" "cloudfront_policy" {
   policy_arn = "arn:aws:iam::aws:policy/CloudFrontReadOnlyAccess"
 }
 
-# Billing & Cost Management Role
-# resource "aws_iam_role" "billing_role" {
-#   name = "billing-role"
-
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Principal = {
-#           Service = "awsbilling.amazonaws.com"
-#         }
-#         Effect = "Allow"
-#         Sid    = ""
-#       },
-#     ]
-#   })
-
-#   tags = {
-#     Name = "billing-role"
-#   }
-# }
-
-# resource "aws_iam_role_policy_attachment" "billing_policy" {
-#   role       = aws_iam_role.billing_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/AWSBillingReadOnlyAccess"
-# }
-
 # CloudWatch & CloudTrail Role (Logging)
 resource "aws_iam_role" "cloudwatch_role" {
   name = "cloudwatch-role"
